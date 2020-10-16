1572131b7fae21031078d158d2557d7048b3112b
Jorge Valdés <jorge121valdes@gmail.com>
2020-10-16 10:05:34 -0600

    Agregada migración para tabla 'notifications'

---
+++ CotizadorH2O/database/migrations/2020_08_10_175131_create_notifications.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotifications extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('action');
+            $table->string('description');
+            $table->string('action_url');
+            $table->unsignedInteger('targeted_user')->nullable();
+            $table->unsignedInteger('created_by')->nullable();
+            $table->unsignedInteger('updated_by')->nullable();
+            $table->boolean('is_status_activated');
+            $table->foreign('targeted_user')->references('id')->on('users');
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('updated_by')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
