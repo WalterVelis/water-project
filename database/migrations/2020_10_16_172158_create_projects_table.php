7f109a71130bc5b5c1a82cdea7b2da44e75faa9b
Jorge Valdés <jorge121valdes@gmail.com>
2020-10-16 16:37:11 -0600

    Agregada migración de proyectos

---
+++ database/migrations/2020_10_16_172158_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->unsignedInteger('status');
+            $table->timestamps();
+            $table->foreign('format_id')->references('id')->on('formats');
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('updated_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
