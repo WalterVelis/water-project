16b7efd44b0f82d56a5cc8abfa2cbd0e5b71bb64
Jorge Valdés <jorge121valdes@gmail.com>
2020-10-23 14:03:58 -0600

    Agregado Controlador de localizaciones (State, Municipality)

---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\MunicipalityState;
+use App\State;
+use Illuminate\Http\Request;
+
+class CountryController extends Controller
+{
+    public function states()
+    {
+        $state = State::all();
+        return view('layouts._country.state', compact('state'));
+    }
+
+    public function municipality($stateId)
+    {
+        $municipality = MunicipalityState::with('municipio')->where('estados_id', $stateId)->get();
+        // dd($municipality);
+        return view('layouts._country.municipality', compact('municipality'));
+    }
+}
