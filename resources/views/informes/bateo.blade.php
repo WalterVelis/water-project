e652ca6e6a0989373acd2ea5249a6245ab27a8db
Jorge Valdés <32805269+jorgev121@users.noreply.github.com>
2021-01-26 10:47:11 -0600

    up

--- resources/views/informes/bateo.blade.php
+++ resources/views/informes/bateo.blade.php
@@ -4,11 +4,20 @@
   'titlePage' => __('Índice de bateo'),
 ])
 
+<style>
+    @media (min-width: 1200px){
+.container {
+    max-width: 1285px!important;
+}
+
+}
+</style>
+
 @section('content')
 <div class="container" style="height: auto;">
   <div class="row justify-content-center">
 
-    <iframe style="border: 0; width: 95%; height: 85vh; margin-top: 80px;" src="https://datastudio.google.com/embed/reporting/4ab22eaa-ecbd-4426-aea2-8f5cd02f0a6a/page/yRetB" frameborder="0" allowfullscreen></iframe>
+    <iframe style="border: 0; width: 98%; height: 85vh; margin-top: 80px;" src="https://datastudio.google.com/embed/reporting/4ab22eaa-ecbd-4426-aea2-8f5cd02f0a6a/page/yRetB" frameborder="0" allowfullscreen></iframe>
 
   </div>
 </div>
--- resources/views/informes/ventas.blade.php
+++ resources/views/informes/ventas.blade.php
@@ -4,11 +4,20 @@
   'titlePage' => __('Informe de ventas'),
 ])
 
+<style>
+    @media (min-width: 1200px){
+.container {
+    max-width: 1285px!important;
+}
+
+}
+</style>
+
 @section('content')
 <div class="container" style="height: auto;">
   <div class="row justify-content-center">
 
-    <iframe style="border: 0; width: 95%; height: 85vh; margin-top: 80px;" src="https://datastudio.google.com/embed/reporting/abf4bc48-fecb-4350-aac9-8e803ca9231a/page/yRetB" frameborder="0" allowfullscreen></iframe>
+    <iframe style="border: 0; width: 98%; height: 85vh; margin-top: 80px;" src="https://datastudio.google.com/embed/reporting/abf4bc48-fecb-4350-aac9-8e803ca9231a/page/yRetB" frameborder="0" allowfullscreen></iframe>
 
   </div>
 </div>
