45a55c61812643dfa71763fc38888215d6b87d45
Jorge Valdés <32805269+jorgev121@users.noreply.github.com>
2021-01-28 11:30:07 -0600

    up

--- resources/views/informes/bateo.blade.php
+++ resources/views/informes/bateo.blade.php
@@ -5,9 +5,40 @@
 ])
 
 <style>
+
+
+
+.card-icon-inf i{
+    color: white;
+    font-size: 1.6em;
+    line-height: 2em;
+    padding-left: 12px;
+}
+.card-icon-inf {
+    background: #0b6696;
+    position: relative;
+    display: block;
+    width: 50px;
+    height: 50px;
+    left: -12px;
+    top: 120px;
+    border-radius: 40px;
+}
+@media (max-width: 800px){
+    .card-icon-inf {
+        display: none!important;
+    }
+}
+
     @media (min-width: 1200px){
 .container {
-    max-width: 1285px!important;
+    max-width: 1485px!important;
+}
+
+@media (max-width: 800px){
+    .card-icon-inf {
+        display: none!important;
+    }
 }
 
 }
@@ -15,9 +46,12 @@
 
 @section('content')
 <div class="container" style="height: auto;">
+    <div class="card-icon-inf">
+        <span style="color:white;"><i class="material-icons">build</i></span>
+    </div>
   <div class="row justify-content-center">
 
-    <iframe style="border: 0; width: 98%; height: 85vh; margin-top: 80px;" src="https://datastudio.google.com/embed/reporting/4ab22eaa-ecbd-4426-aea2-8f5cd02f0a6a/page/yRetB" frameborder="0" allowfullscreen></iframe>
+    <iframe style="border-radius: 15px;border: 0; width: 98%; height: 85vh; margin-top: 80px;" src="https://datastudio.google.com/embed/reporting/4ab22eaa-ecbd-4426-aea2-8f5cd02f0a6a/page/yRetB" frameborder="0" allowfullscreen></iframe>
 
   </div>
 </div>
